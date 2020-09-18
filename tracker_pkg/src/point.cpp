a6c565354f44e7dff2c8022f027dc0b7e243adbf
yangkun <1269769296@qq.com>
2020-09-18 15:47:04 +0800

    finished calc depth

--- tracker_pkg/src/point.cpp
+++ tracker_pkg/src/point.cpp
@@ -22,6 +22,7 @@ ros::Publisher pub;
 geometry_msgs::PoseStamped depth;
 int depth_w = 640;
 int depth_h = 480;
+cv::Point2i center_offset(10, 10);
 
 void pos_image_cb(const std_msgs::Float32MultiArray &input)
 {
@@ -35,7 +36,7 @@ void pos_image_cb(const std_msgs::Float32MultiArray &input)
         perspectiveTransform(points_projection, points_back_projection, homography_from_file);
         center_point.x = int(points_back_projection[0].x);
         center_point.y = int(points_back_projection[0].y);
-        cout << "nano: " << points_projection[0] << "  --->  " << "D435i: " << center_point << endl;
+        // cout << "nano: " << points_projection[0] << "  --->  " << "D435i: " << center_point << endl;
     }
 }
 
@@ -47,40 +48,53 @@ void depth_Callback(const sensor_msgs::ImageConstPtr &depth_msg)
     }
     else 
     {
-        try
-        {
-            cv::imshow("depth_view", cv_bridge::toCvShare(depth_msg, sensor_msgs::image_encodings::TYPE_32FC1)->image);
-            depth_ptr = cv_bridge::toCvCopy(depth_msg, sensor_msgs::image_encodings::TYPE_32FC1);
-            cv::waitKey(1);
-        }
-        catch (cv_bridge::Exception &e)
-        {
-            ROS_ERROR("Could not convert from '%s' to 'mono16'.", depth_msg->encoding.c_str());
-        }
+        // cv::imshow("depth_view", cv_bridge::toCvShare(depth_msg, sensor_msgs::image_encodings::TYPE_32FC1)->image);
+        depth_ptr = cv_bridge::toCvCopy(depth_msg, sensor_msgs::image_encodings::TYPE_32FC1);
+        double minval, maxval;
+        cv::minMaxIdx(depth_ptr -> image, &minval, &maxval);
+        cv::Mat adjMap, hotMap;
+        //expand your range to 0 and 255
+        depth_ptr -> image.convertTo(adjMap, CV_8UC1, 255/(maxval-minval), -minval);
+        applyColorMap(adjMap, hotMap, COLORMAP_HOT);
+        // Rectangle: img, left-top, right-bottom, color, line-width, line-type, point-type 
+        cv::rectangle(hotMap, center_point-center_offset, center_point+center_offset, Scalar(255,0,0),3,8,0);
         depth_pic = depth_ptr->image;
 
         float depth_sum = 0;
         int cnt = 0;
-        for (int i=center_point.x-5; i<=center_point.x+5; i++) {
-            for (int j=center_point.y-5; j<=center_point.y+5; j++) {
-                float dd = depth_pic.ptr<float>(i)[j];
+        for (int i=center_point.x-center_offset.x; i<=center_point.x+center_offset.x; i++) {
+            for (int j=center_point.y-center_offset.y; j<=center_point.y+center_offset.y; j++) {
+                // border detection
+                if (i<0 || i>=depth_w || j<0 || j>=depth_h)   continue;
+                float dd = depth_pic.ptr<float>(j)[i];
                 if (dd > 10 && dd < 8182) {
                     cnt++;
-                    // cout << i << ", " << j << ": " << dd << endl;
+                    // cout << j << ", " << i << ": " << dd << endl;
                     depth_sum += dd;
                 }
             }
         }
-        depth.pose.position.x = depth.pose.position.y = depth.pose.position.z = depth_sum/cnt;
+        if (cnt == 0) {
+            depth.pose.position.x = depth.pose.position.y = depth.pose.position.z = -1;
+        }
+        else {
+            depth.pose.position.x = depth.pose.position.y = depth.pose.position.z = depth_sum/cnt;
+        }
+        // cout << depth.pose.position.x << endl << endl << endl;
+
+        putText(hotMap, "depth: "+std::to_string(depth.pose.position.x), Point(100,100),
+                FONT_HERSHEY_SIMPLEX, 0.7, Scalar(255,0,0), 2, CV_AA);
+        imshow("DepthImage", hotMap);
+        cv::waitKey(1);
     }
     
     pub.publish(depth);
 
     // output some info about the depth image in cv format
-    cout << "output some info about the depth image in cv format" << endl;
-    cout << "rows of the depth image = " << depth_pic.rows << endl;
-    cout << "cols of the depth image = " << depth_pic.cols << endl;
-    cout << "type of depth_pic's element = " << depth_pic.type() << endl;
+    // cout << "output some info about the depth image in cv format" << endl;
+    // cout << "rows of the depth image = " << depth_pic.rows << endl;
+    // cout << "cols of the depth image = " << depth_pic.cols << endl;
+    // cout << "type of depth_pic's element = " << depth_pic.type() << endl;
 }
 
 int main(int argc, char **argv)
