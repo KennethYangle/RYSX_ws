5e9c3e8dd913576df85e822230ffcc92ca247556
yangkun <1269769296@qq.com>
2020-09-19 12:28:17 +0800

    update all.sh

--- offboard_pkg/shell/all.sh
+++ offboard_pkg/shell/all.sh
@@ -2,9 +2,9 @@
 
 # Set FC into Hex+ mode
 ~/DartTracker/DartTracker 127.0.0.1 0.42 0.06 & PID0=$!
-sleep 10s
-roslaunch tracker_pkg tracker.launch & PID3=$!
 sleep 5s
+roslaunch tracker_pkg calc_depth.launch & PID3=$!
+sleep 10s
 roslaunch mavros px4.launch fcu_url:="/dev/ttyACM0:57600" & PID1=$!
 sleep 10s
 # rosrun car_pkg car.sh & PID2=$!
