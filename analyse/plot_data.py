7f43c949b1488f63ff0a3756a8f4a92329e6689d
yangkun <1269769296@qq.com>
2020-10-27 17:10:54 +0800

    upgrade plot_data.py with some parameters. python plot_data.py -h for more info

--- analyse/plot_data.py
+++ analyse/plot_data.py
@@ -26,6 +26,7 @@ def main(args):
         cnt += 1
     print(data_len)
     # print(raw_datas)
+
     datas = [[[] for i in range(len(raw_datas[v][0]))] for v in range(nvar)]
     for v in range(nvar):
         for i in range(len(raw_datas[v])):
@@ -34,21 +35,44 @@ def main(args):
                 datas[v][j].append(raw_datas[v][i][j])
     # print(datas)
     
-    for i in range(len(datas[0])):
-        plt.figure(i)
+    if args.self:
         for v in range(nvar):
-            if i < len(datas[v]):
-                plt.plot(index[v], datas[v][i], label="{}[{}]".format(args.variable[v], i))
-            else:
-                plt.plot(index[v], datas[v][0], label="{}[{}]".format(args.variable[v], 0))
-        plt.legend()
-    plt.show()
+            plt.figure(v)
+            for i in range(len(datas[v])):
+                plt.plot(index[v], datas[v][i], label="{}[{}]".format(args.variable[v], i), linewidth=args.linewidth)
+            plt.legend()
+        plt.show()
+
+    if args.plotxy:
+        for v in range(nvar):
+            plt.figure(v)
+            plt.plot(datas[v][0], datas[v][1], label="{}[x:y]".format(args.variable[v]), linewidth=args.linewidth)
+            if args.range is not None:
+                arange = [float(a) for a in re.findall(r'-?\d+\.?\d*e?[-+]?\d*', args.range)]
+                plt.axis(arange)
+            plt.legend()
+        plt.show()
+
+    if not args.self and not args.plotxy:
+        for i in range(len(datas[0])):
+            plt.figure(i)
+            for v in range(nvar):
+                if i < len(datas[v]):
+                    plt.plot(index[v], datas[v][i], label="{}[{}]".format(args.variable[v], i), linewidth=args.linewidth)
+                else:
+                    plt.plot(index[v], datas[v][0], label="{}[{}]".format(args.variable[v], 0), linewidth=args.linewidth)
+            plt.legend()
+        plt.show()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('log', help='log file path')
     parser.add_argument('variable', nargs='+', help='variable to plot')
+    parser.add_argument('-s', '--self', action='store_true', help='draw the various components of the variable on a figure')
+    parser.add_argument('-p', '--plotxy', action='store_true', help='draw a 2-dimensional graph')
+    parser.add_argument('-l', '--linewidth', default=2, type=float, help='line width')
+    parser.add_argument('-r', '--range', default=None, help='axises range, work with plotxy. usage: "xmin xmax ymin ymax"')
     args = parser.parse_args()
     print(args)
     main(args)
