99c12b7e09c04d04aaab36c761852e1c9c57bff7
yangkun <1269769296@qq.com>
2020-11-03 20:41:15 +0800

    update

--- analyse/plot_multiple_file.py
+++ analyse/plot_multiple_file.py
@@ -18,6 +18,7 @@ def main(args):
     nlog = len(args.log)
     raw_datas = [[] for l in range(nlog)]
     index = [[] for l in range(nlog)]
+    begin_time = [0 for l in range(nlog)]
 
     for l in range(nlog):
         f = open(args.log[l])
@@ -27,12 +28,16 @@ def main(args):
             if line.startswith("time:"):
                 tmp = [float(a) for a in re.findall(r'-?\d+\.?\d*e?[-+]?\d*', line)]
                 time = tmp[0]
+                if begin_time[l] == 0: begin_time[l] = time
             if time < tmin or time > tmax:
                 continue
             if line.startswith(args.variable+":"):
                 tmp = [float(a) for a in re.findall(r'-?\d+\.?\d*e?[-+]?\d*', line)]
                 raw_datas[l].append(tmp)
-                index[l].append(time)
+                if args.align:
+                    index[l].append(time - begin_time[l])
+                else:
+                    index[l].append(time)
     # print(raw_datas)
 
     datas = [[[] for i in range(len(raw_datas[l][0]))] for l in range(nlog)]
@@ -44,6 +49,10 @@ def main(args):
     # print(datas)
 
     if args.plotxy:
+        fig, ax = plt.subplots()
+        if args.grid:
+            plt.plot([0,640],[240,240], color='#A5A5A5', linewidth=2)
+            plt.plot([320,320],[0,480], color='#A5A5A5', linewidth=2)
         for l in range(nlog):
             if args.label is not None:
                 plt.plot(datas[l][0], datas[l][1], label=args.label[l], linewidth=args.linewidth)
@@ -60,12 +69,18 @@ def main(args):
             plt.ylabel(args.ylabel)
         plt.legend()
         if args.grid:
+            plt.xticks(np.arange(0,642,80))
+            plt.yticks(np.arange(0,481,80))
             plt.grid()
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+        fig.savefig("err.png", dpi=1200)
         plt.show()
 
     if not args.self and not args.plotxy:
         for i in range(len(datas[0])):
-            plt.figure(i)
+            fig = plt.figure(i)
+            ax = fig.add_subplot(111)
             for l in range(nlog):
                 if args.label is not None:
                     plt.plot(index[l], datas[l][i], label=args.label[l], linewidth=args.linewidth)
@@ -83,6 +98,9 @@ def main(args):
             plt.legend()
             if args.grid:
                 plt.grid()
+            ax.spines['top'].set_visible(False)
+            ax.spines['right'].set_visible(False)
+            fig.savefig("{}.png".format(i), dpi=1200)
         plt.show()
 
 
@@ -93,6 +111,7 @@ if __name__ == "__main__":
     parser.add_argument('-s', '--self', action='store_true', help='draw the various components of the variable on a figure')
     parser.add_argument('-p', '--plotxy', action='store_true', help='draw a 2-dimensional graph')
     parser.add_argument('-g', '--grid', action='store_true', help='show grid')
+    parser.add_argument('-a', '--align', action='store_true', help='align the time axis')
     parser.add_argument('-l', '--linewidth', default=2, type=float, help='line width')
     parser.add_argument('-r', '--range', default=None, help='axises range, work with plotxy. usage: "xmin xmax ymin ymax"')
     parser.add_argument('-t', '--time', default=None, help='time range. usage: "tmin tmax" or tmin')
